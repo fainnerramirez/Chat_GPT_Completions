8902fbec4681ee294147480036ccc4ff89c22f2d
Fainner Ramirez <framirez@godoycordoba.com>
2023-11-19 10:29:13 -0500

    Vercel analitys

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react';
 
 const Root = () => {
   return (
     <div>
       <Outlet />
+      <Analytics />
     </div>
   )
 }
